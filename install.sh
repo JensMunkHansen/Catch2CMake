d36307995f1fa1ed401ad866f8ae5bf220f91e94
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-22 22:37:05 +0200

    ..

--- install.sh
+++ install.sh
@@ -3,5 +3,21 @@
 rm -Rf $(pwd)/install
 rm -Rf $(pwd)/build/library
 
-cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release
-cmake --build build/library --parallel 8
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 shared/static"
+    exit 1
+fi
+
+# Convert parameter to lowercase for case-insensitive comparison
+param=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+if [ "$param" = "shared" ]; then
+    echo "Shared."
+    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+    
+else
+    echo "Static."
+    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+fi
+
+cmake --build build/library --parallel 16
