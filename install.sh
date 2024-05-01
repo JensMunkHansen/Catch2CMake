d2d717c053d65d157672d2d8f2286a66d5c8decd
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-05-01 18:14:08 +0200

    ..

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-rm -Rf $(pwd)/install
-rm -Rf $(pwd)/build/library
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 shared/static"
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [wasm|static|shared]"
     exit 1
 fi
 
@@ -13,10 +10,20 @@ param=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
 if [ "$param" = "shared" ]; then
     echo "Shared."
-    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-else
+    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON
+elif [ "$param" = "static" ]; then
     echo "Static."
-    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+    cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build/library --parallel 16
+elif [ "$param" = "wasm" ]; then (
+    echo "Emscripten."
+    source "/home/jmh/github/emsdk/emsdk_env.sh"
+    emcmake cmake -Bbuild/library -H./Library -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build/library --parallel 16
+)
+else
+    echo "Invalid argument. Usage: $0 [wasm|static|shared]"
+    exit 1    
 fi
 
 cmake --build build/library --parallel 16
--- usage.sh
+++ usage.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-rm -Rf $(pwd)/build/usage
+# Check if no arguments are provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [wasm|static|shared]"
+    exit 1
+fi
 
-cmake -Bbuild/usage -H./Usage -DCMAKE_FIND_ROOT_PATH=$(pwd)/install
-cmake --build build/usage --parallel 8
+# Convert parameter to lowercase for case-insensitive comparison
+arg=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
-./build/usage/main
+if [ "$arg" = "static" ]; then
+    echo "Static library"
+    cmake -Bbuild/usage -H./Usage -DCMAKE_FIND_ROOT_PATH=$(pwd)/install -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build/usage --parallel 16
+    ./build/usage/main
+elif [ "$arg" = "shared" ]; then
+    echo "Shared library"
+    cmake -Bbuild/usage -H./Usage -DCMAKE_FIND_ROOT_PATH=$(pwd)/install -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build/usage --parallel 16
+    ./build/usage/main
+elif [ "$arg" = "wasm" ]; then
+(
+    echo "Emscripten"
+    source "/home/jmh/github/emsdk/emsdk_env.sh"
+    emcmake cmake -Bbuild/usage -H./Usage -DCMAKE_FIND_ROOT_PATH=$(pwd)/install -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build/usage --parallel 16
+)
+else
+    echo "Invalid argument. Usage: $0 [wasm|static|shared]"
+    exit 1
+fi
+
+
