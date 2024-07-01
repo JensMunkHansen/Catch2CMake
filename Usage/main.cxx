bcfaca80b5ef7cbfdee699e3ac1865187c0b5a86
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-07-01 19:35:41 +0200

    ..

--- Usage/main.cxx
+++ Usage/main.cxx
@@ -1,10 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cstring>
 #include <fstream>
+#include <iostream>
 
 TEST_CASE("TestMe")
 {
-  const std::string filepath = "/input.txt";
+  const std::string filepath = "/assets/input.txt";
   std::ifstream ifs(filepath, std::ios::binary | std::ios::ate);  
 
   if(!ifs)
@@ -15,12 +16,24 @@ TEST_CASE("TestMe")
   
   auto size = std::size_t(end - ifs.tellg());
 
+  std::cout << size << std::endl;
   if(size == 0) // avoid undefined behavior 
     REQUIRE(0 == 1);
   
-  std::vector<std::byte> buffer(size);
+  const std::string filepath2 = "/assets/input2.txt";
+  std::ifstream ifs2(filepath2, std::ios::binary | std::ios::ate);  
 
-  if(!ifs.read((char*)buffer.data(), buffer.size()))
-    throw std::runtime_error(filepath + ": " + std::strerror(errno));
+  if(!ifs2)
+    throw std::runtime_error(filepath2 + ": " + std::strerror(errno));
+
+  end = ifs2.tellg();
+  ifs2.seekg(0, std::ios::beg);
+  
+  size = std::size_t(end - ifs2.tellg());
+
+  std::cout << size << std::endl;
+  
   REQUIRE(1 == 1);
+
+  
 }
