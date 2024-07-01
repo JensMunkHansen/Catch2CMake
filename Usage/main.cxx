298c8d58d32d9c56e7e42e30fd2fd0d1cb2683b6
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-07-01 18:48:08 +0200

    ..

--- Usage/main.cxx
+++ Usage/main.cxx
@@ -1,7 +1,28 @@
 #include <catch2/catch_test_macros.hpp>
-
+#include <cstring>
+#include <fstream>
 
 TEST_CASE("TestMe")
 {
+  const std::string filepath = "/input.txt";
+  std::ifstream ifs(filepath, std::ios::binary | std::ios::ate);  
+
+  if(!ifs)
+    throw std::runtime_error(filepath + ": " + std::strerror(errno));
+#if 0
+
+  auto end = ifs.tellg();
+  ifs.seekg(0, std::ios::beg);
+  
+  auto size = std::size_t(end - ifs.tellg());
+
+  if(size == 0) // avoid undefined behavior 
+    REQUIRE(0 == 1);
+  
+  std::vector<std::byte> buffer(size);
+
+  if(!ifs.read((char*)buffer.data(), buffer.size()))
+    throw std::runtime_error(filepath + ": " + std::strerror(errno));
+#endif
   REQUIRE(1 == 1);
 }
